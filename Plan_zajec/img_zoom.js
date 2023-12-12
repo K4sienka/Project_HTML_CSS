68dba8924bcbb34644bcbff99822894c4edd1acb
KarolinaMurzyn <karola.murzyn@gmail.com>
2023-12-12 18:12:32 +0100

    update
    dziwne próby zmiany rozwalenie strny

--- Plan_zajec/img_zoom.js
+++ Plan_zajec/img_zoom.js
@@ -29,3 +29,26 @@ function przeniesDoKalendarza(id) {
     }
     
 }
+
+
+let slideIndex = 1;
+
+function showSlides(n) {
+    const slides = document.querySelectorAll('.slides img');
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+showSlides(slideIndex);
