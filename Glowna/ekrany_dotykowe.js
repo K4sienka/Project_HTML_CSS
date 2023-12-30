f5c42131534bb6866577f56d07f359dc19a20cc2
K4sienka <147631514+K4sienka@users.noreply.github.com>
2023-12-31 00:31:29 +0100

    gotowe_dla_telefonow

---
+++ Glowna/ekrany_dotykowe.js
@@ -0,0 +1,32 @@
+//dla menu
+if ($(window).width() < 1340) {
+    $('.menu').on('click', function (e) {
+        // Sprawdź, czy element posiada podmenu
+        if ($(this).next('.podmenu').length > 0) {
+            e.preventDefault(); // Zapobiegnij domyślnej akcji kliknięcia
+
+            // Sprawdź, czy podmenu jest aktualnie schowane
+            if (!$(this).data('expanded')) {
+                // Jeśli schowane, to rozwinięcie
+                $(this).next('.podmenu').slideDown();
+                $(this).data('expanded', true);
+            } else {
+                // Jeśli rozwinięte, to zwiniecie
+                $(this).next('.podmenu').slideUp();
+                $(this).data('expanded', false);
+            }
+        }
+    });
+}
+
+//dla uczelni
+function toggleSquareSize(squareId) {
+    var square = document.getElementById(squareId);
+
+    if (square.classList.contains('active')) {
+        square.classList.remove('active');
+    } else {
+        square.classList.add('active');
+    }
+}
+
