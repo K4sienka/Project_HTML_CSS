e65b6e35fbb3bf8caa490f400edb563fe0f805fd
K4sienka <147631514+K4sienka@users.noreply.github.com>
2024-01-10 00:17:50 +0100

    x

--- Zarzad/link_scroll.js
+++ Zarzad/link_scroll.js
@@ -3,6 +3,16 @@ function redirectToPage(url) {
     window.open(url, '_blank');
 }
 
+//przesuwanie po stronie
+function scrollToElement(elementId) {
+  var element = document.getElementById(elementId);
+
+  if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+
 //przewijanie zdjec
 /**
   Swiper:
