56628167268979345bf2048b4a5f7ac636a6dc2e
KarolinaMurzyn <karola.murzyn@gmail.com>
2024-01-10 02:22:21 +0100

    baza kontaktowa

---
+++ Sesja/Egzaminy/slide_2.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+    $('.note').hide(); // Ukrycie wszystkich notatek na starcie
+
+    $('.hideButton').click(function() {
+        const note = $(this).parent().prev('.note');
+        note.slideToggle(function() {
+            const button = $(this).next('.button').find('.hideButton');
+            const parentClass = $(this).parent().attr('class').split(' ')[0]; // Pobranie pierwszej klasy rodzica
+            
+            if ($(this).is(':visible')) {
+                button.text('Kliknij żeby zwinąć');
+            } else {
+                
+                if (parentClass === 'Teoria_sygnałów_dyskretnych') {
+                    button.text('Teoria sygnałów dyskretnych');
+                } else if (parentClass === 'Bazy_danych') {
+                    button.text('Bazy danych');
+                } else {
+                    button.text('T');
+                }
+            }
+        });
+    });
+});
+
+
+
