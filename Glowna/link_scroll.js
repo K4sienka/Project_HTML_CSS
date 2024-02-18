a2bae10fbe0c20c02cfc2eb8bf6b07a4e5162068
K4sienka <147631514+K4sienka@users.noreply.github.com>
2024-02-18 01:33:56 +0100

    gotowe

--- Glowna/link_scroll.js
+++ Glowna/link_scroll.js
@@ -3,6 +3,12 @@ function redirectToPage(url) {
     window.open(url, '_blank');
 }
 
+//link do mapki
+document.getElementById('map').addEventListener('click', function() 
+{
+    window.open('../Glowna/photos/mapka.jpg', '_blank');
+});
+
 //przesuwanie po stronie
 function scrollToElement(elementId) {
     var element = document.getElementById(elementId);
